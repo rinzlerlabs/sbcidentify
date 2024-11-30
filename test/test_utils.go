f0d4e341d40d856560db6fc7715cb5f693d87e62
TheGreatCO <thegreatco@thegreatco.com>
2024-11-30 13:26:45 -0500

    add missing test package

---
+++ test/test_utils.go
@@ -0,0 +1,57 @@
+package test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/rinzlerlabs/sbcidentify"
+	"github.com/rinzlerlabs/sbcidentify/boardtype"
+)
+
+type test struct {
+	requiresRoot      bool
+	requiredBoardType *boardtype.SBC
+	requiresSbc       bool
+}
+
+func Test() *test {
+	return &test{}
+}
+
+func (t *test) RequiresRoot() *test {
+	t.requiresRoot = true
+	return t
+}
+
+func (t *test) RequiresBoardType(boardType boardtype.SBC) *test {
+	t.requiredBoardType = &boardType
+	return t
+}
+
+func (t *test) RequiresSbc() *test {
+	t.requiresSbc = true
+	return t
+}
+
+func (t *test) ShouldSkip(test *testing.T) {
+	if t.requiresSbc {
+		boardType, err := sbcidentify.GetBoardType()
+		if err != nil {
+			test.Log(err)
+		}
+		if boardType == nil {
+			test.Skip("Test requires physical SBC, not running on SBC")
+		}
+	}
+
+	if t.requiredBoardType != nil && !sbcidentify.IsBoardType(*t.requiredBoardType) {
+		test.Skipf("Test requires board type %v", (*t.requiredBoardType).GetPrettyName())
+	}
+	if t.requiresRoot && !IsRoot() {
+		test.Skip("Test requires root, not running as root")
+	}
+}
+
+func IsRoot() bool {
+	return os.Geteuid() == 0
+}
---
+++ test/test_utils_test.go
@@ -0,0 +1,61 @@
+package test
+
+import (
+	"log/slog"
+	"testing"
+
+	"github.com/rinzlerlabs/sbcidentify"
+	"github.com/rinzlerlabs/sbcidentify/boardtype"
+)
+
+func TestShouldSkip(t *testing.T) {
+	sbcidentify.SetLogLevel(slog.LevelDebug)
+	tests := []struct {
+		name       string
+		setup      func() *test
+		shouldSkip bool
+		msg        string
+	}{
+		{
+			name: "No requirements",
+			setup: func() *test {
+				return Test()
+			},
+			shouldSkip: false,
+			msg:        "Test has no requirements",
+		},
+		{
+			name: "Requires SBC but no SBC present",
+			setup: func() *test {
+				return Test().RequiresSbc()
+			},
+			shouldSkip: true,
+			msg:        "Test requires physical SBC",
+		},
+		{
+			name: "Requires specific board type but different board type present",
+			setup: func() *test {
+				return Test().RequiresBoardType(boardtype.RaspberryPi3B)
+			},
+			shouldSkip: true,
+			msg:        "Test requires board type",
+		},
+		{
+			name: "Requires root but not running as root",
+			setup: func() *test {
+				return Test().RequiresRoot()
+			},
+			shouldSkip: true,
+			msg:        "Test requires root",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.setup().ShouldSkip(t)
+			if tt.shouldSkip {
+				t.Errorf("%v should have skipped, but did not", tt.name)
+			}
+		})
+	}
+}
