f4d98dd3cacf31a464aacff1212203db73f4f298
John Walicki <johnwalicki@gmail.com>
2025-11-17 16:04:02 -0500

    Add support for Compute Modules
    Signed-off-by: John Walicki <johnwalicki@gmail.com>

--- boardtype/raspberrypi.go
+++ boardtype/raspberrypi.go
@@ -13,9 +13,17 @@ var (
 	RaspberryPi4B4GB  = BoardType{Manufacturer: "Raspberry Pi", Model: "Raspberry Pi", SubModel: "4B", RAM: 4096, BaseModel: &RaspberryPi4B}
 	RaspberryPi4B8GB  = BoardType{Manufacturer: "Raspberry Pi", Model: "Raspberry Pi", SubModel: "4B", RAM: 8192, BaseModel: &RaspberryPi4B}
 	RaspberryPi4400   = BoardType{Manufacturer: "Raspberry Pi", Model: "Raspberry Pi", SubModel: "4 400", RAM: 4096, BaseModel: &RaspberryPi4B}
+	RaspberryPiCM41GB = BoardType{Manufacturer: "Raspberry Pi", Model: "Raspberry Pi", SubModel: "Compute Module 4", RAM: 1024, BaseModel: &RaspberryPi4B}
+	RaspberryPiCM42GB = BoardType{Manufacturer: "Raspberry Pi", Model: "Raspberry Pi", SubModel: "Compute Module 4", RAM: 2048, BaseModel: &RaspberryPi4B}
+	RaspberryPiCM44GB = BoardType{Manufacturer: "Raspberry Pi", Model: "Raspberry Pi", SubModel: "Compute Module 4", RAM: 4096, BaseModel: &RaspberryPi4B}
+	RaspberryPiCM48GB = BoardType{Manufacturer: "Raspberry Pi", Model: "Raspberry Pi", SubModel: "Compute Module 4", RAM: 8192, BaseModel: &RaspberryPi4B}
 	RaspberryPi5      = BoardType{Manufacturer: "Raspberry Pi", Model: "Raspberry Pi", SubModel: "", RAM: 0, BaseModel: &RaspberryPi}
 	RaspberryPi5B     = BoardType{Manufacturer: "Raspberry Pi", Model: "Raspberry Pi", SubModel: "5B", RAM: 0, BaseModel: &RaspberryPi5}
 	RaspberryPi5B2GB  = BoardType{Manufacturer: "Raspberry Pi", Model: "Raspberry Pi", SubModel: "5B", RAM: 2048, BaseModel: &RaspberryPi5B}
 	RaspberryPi5B4GB  = BoardType{Manufacturer: "Raspberry Pi", Model: "Raspberry Pi", SubModel: "5B", RAM: 4096, BaseModel: &RaspberryPi5B}
 	RaspberryPi5B8GB  = BoardType{Manufacturer: "Raspberry Pi", Model: "Raspberry Pi", SubModel: "5B", RAM: 8192, BaseModel: &RaspberryPi5B}
+	RaspberryPiCM51GB = BoardType{Manufacturer: "Raspberry Pi", Model: "Raspberry Pi", SubModel: "Compute Module 5", RAM: 1024, BaseModel: &RaspberryPi5B}
+	RaspberryPiCM52GB = BoardType{Manufacturer: "Raspberry Pi", Model: "Raspberry Pi", SubModel: "Compute Module 5", RAM: 2048, BaseModel: &RaspberryPi5B}
+	RaspberryPiCM54GB = BoardType{Manufacturer: "Raspberry Pi", Model: "Raspberry Pi", SubModel: "Compute Module 5", RAM: 4096, BaseModel: &RaspberryPi5B}
+	RaspberryPiCM58GB = BoardType{Manufacturer: "Raspberry Pi", Model: "Raspberry Pi", SubModel: "Compute Module 5", RAM: 8192, BaseModel: &RaspberryPi5B}
 )
